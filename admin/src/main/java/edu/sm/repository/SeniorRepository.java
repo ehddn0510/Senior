8df82f0d59230ce7297acd7d76dcaec2bf9a29b1
daniel <pejqueen7@gmail.com>
2024-11-11 11:19:08 +0900

    feat: SeniorRepository 및 MyBatis 매퍼 추가
    - Senior 데이터 조회를 위한 SeniorRepository 인터페이스 생성
    - MyBatis 매퍼 파일(seniormapper.xml)에 Senior 조회 쿼리 추가

---
+++ admin/src/main/java/edu/sm/repository/SeniorRepository.java
@@ -0,0 +1,16 @@
+package edu.sm.repository;
+
+import edu.sm.frame.SMRepository;
+import edu.sm.model.Senior;
+import edu.sm.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface SeniorRepository extends SMRepository<Integer, Senior>  {
+    Senior selectOne(Integer seniorId);
+    List<Senior> findAll();
+}
