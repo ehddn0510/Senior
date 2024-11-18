2aa09e24eb64a213123b1ec63060b1fff99f546a
daniel <pejqueen7@gmail.com>
2024-11-18 18:45:28 +0900

    feat: Careworker 모델 및 UI 개선
    - Careworker 모델의 필드 타입 및 DateTimeFormat 어노테이션 추가
    - Center 레이아웃 대시보드 UI 개선

--- admin/src/main/java/edu/sm/model/Careworker.java
+++ admin/src/main/java/edu/sm/model/Careworker.java
@@ -7,31 +7,35 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Careworker {
-    private int cwId;
+    private Integer cwId;
     private String cwUsername;
     private String cwPassword;
     private String cwTel;
     private String cwEmail;
     private String cwName;
     private Gender cwGender;
-    private LocalDate cwBirthday;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date cwBirthday;
     private String cwZipcode;
     private String cwStreetAddr;
     private String cwDetailAddr1;
     private String cwDetailAddr2;
-    private LocalDateTime cwRdate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date cwRdate;
     private CwStatus cwStatus;
     private String cwProfile;
     private DayOfWeek cwHoliday;
     private String cwIntro;
-    private int cwExperience;
+    private Integer cwExperience;
 }
