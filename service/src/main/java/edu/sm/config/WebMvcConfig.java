fda935ba61d9c0d0eee5b38be6bd1423395f5cac
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-11-26 14:59:30 +0900

    refactor: WebMvcConfig 수정

--- service/src/main/java/edu/sm/config/WebMvcConfig.java
+++ service/src/main/java/edu/sm/config/WebMvcConfig.java
@@ -17,16 +17,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        String seniorDir = Paths.get(imgDir, "senior").toString();
-        String careworkerDir = Paths.get(imgDir, "careworker").toString();
-        String userDir = Paths.get(imgDir, "user").toString();
-        seniorDir = seniorDir + "/";
-        careworkerDir = careworkerDir + "/";
-        userDir = userDir + "/";
+        String baseDir = imgDir.replace("file:", "").trim();
+        String seniorDir = Paths.get(baseDir, "senior").toString().replace("\\", "/") + "/";
+        String careworkerDir = Paths.get(baseDir, "careworker").toString().replace("\\", "/") + "/";
+        String userDir = Paths.get(baseDir, "user").toString().replace("\\", "/") + "/";
 
-        registry.addResourceHandler("/imgs/senior/**").addResourceLocations(seniorDir);
-        registry.addResourceHandler("/imgs/careworker/**").addResourceLocations(careworkerDir);
-        registry.addResourceHandler("/imgs/user/**").addResourceLocations(userDir);
+        registry.addResourceHandler("/imgs/senior/**").addResourceLocations("file:" + seniorDir);
+        registry.addResourceHandler("/imgs/careworker/**").addResourceLocations("file:" + careworkerDir);
+        registry.addResourceHandler("/imgs/user/**").addResourceLocations("file:" + userDir);
     }
-
 }
